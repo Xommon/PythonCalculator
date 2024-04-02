50fcdb3b7f4189e259a2e099d327a59fcf1dc654
Micheal Quentin <mquenti3@gmail.com>
2024-04-02 12:24:59 -0500

    Button Layout

---
+++ calculator.py
@@ -0,0 +1,38 @@
+from tkinter import*
+import math
+
+# Create window
+window = Tk()
+entry = Entry(window, width=10).grid(row=0, column=0)
+
+# Button methods
+def type_number(button):
+    print("number")
+
+def clear():
+    print("clear")
+
+def operation():
+    print("operation")
+
+def decimal():
+    print("decimal")
+
+def equals():
+    print("equals")
+
+# Create calculator buttons
+for i in range(10):
+    if i == 0:
+        button = Button(window, text=str(i), command=lambda: type_number(button), width=10, height=5).grid(row=4, column=1)
+    else:
+        index = i-1
+        button = Button(window, text=str(i), command=lambda: type_number(button), width=10, height=5).grid(row=math.floor(index/3)+1, column=index%3)
+for i in range(4):
+    operations = ['+', '-', '*', '/']
+    button = Button(window, text=operations[i], command=operation, width=10, height=5).grid(row=i+1, column=4)
+button = Button(window, text='C', command=clear, width=10, height=5).grid(row=4, column=0)
+button = Button(window, text='.', command=decimal, width=10, height=5).grid(row=4, column=2)
+button = Button(window, text='=', command=equals, width=10, height=5).grid(row=5, column=1)
+
+window.mainloop()
